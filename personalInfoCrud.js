1362f45c0d202029df5d9c97381cf39df1c67f39
onticinc <onticinc@gmail.com>
2021-12-01 11:29:03 -0700

    added more personal data on personalInfoCrud.js

--- personalInfoCrud.js
+++ personalInfoCrud.js
@@ -16,8 +16,100 @@ PersonalInfo.create({
         console.log('Error creating info', error)
     })
 
+PersonalInfo.create({
+    name: 'Erica Stevenson',
+    birthday: '12-11-1980',
+    gender: 'female',
+    email: 'cool_email_2@email.com',
+    phone: '877-5313'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
 
+PersonalInfo.create({
+    name: 'Steven Stevenson',
+    birthday: '2-2-1980',
+    gender: 'male',
+    email: 'cool_email_3@email.com',
+    phone: '877-5310'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
 
+PersonalInfo.create({
+    name: 'Sarah Stevenson',
+    birthday: '3-13-1983',
+    gender: 'female',
+    email: 'cool_email_3@email.com',
+    phone: '877-5312'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
+
+PersonalInfo.create({
+    name: 'Mary Stevenson',
+    birthday: '12-12-1982',
+    gender: 'male',
+    email: 'cool_email_5@email.com',
+    phone: '877-5312'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
+PersonalInfo.create({
+    name: 'Tory Stevenson',
+    birthday: '10-2-1985',
+    gender: 'female',
+    email: 'cool_email_6@email.com',
+    phone: '877-5315'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
+PersonalInfo.create({
+    name: 'Tyler Stevenson',
+    birthday: '8-10-1980',
+    gender: 'male',
+    email: 'cool_email_16@email.com',
+    phone: '877-5320'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
+PersonalInfo.create({
+    name: 'Frank Stevenson',
+    birthday: '10-15-1990',
+    gender: 'male',
+    email: 'cool_email_18@email.com',
+    phone: '877-5320'
+})
+    .then(function (newPersonalInfo) {
+        console.log(newPersonalInfo.toJSON());
+    })
+    .catch(function (error) {
+        console.log('Error creating info', error)
+    })
 // ******** Update Info
 
 PersonalInfo.update({
